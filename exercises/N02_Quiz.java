e70b7d04a1dc040e026236965980255d475c4456
slow3586 <6337520+slow3586@users.noreply.github.com>
2022-03-02 18:47:26 +0700

    Updated exercises: N02, N02 Alt

--- exercises/N02_Quiz.java
+++ exercises/N02_Quiz.java
@@ -1,39 +1,64 @@
 import java.util.Scanner;
 
 public class N02_Quiz {
+
+    //Вариант с вводом через текст ответа
     public static void main(String[] args) {
-        String[][] answerTexts = new String[][] {
+
+        //Тексты ответов для вопросов
+        String[][] answerTexts = new String[][]{
                 {"Вторник", "Четверг", "Суббота"},
                 {"Суббота", "Воскресенье", "Понедельник"},
                 {"10", "7", "25"}
         };
-        String[] questionTexts = new String[]{"Какой из указанных дней недели - выходной?", "Какой из указанных дней недели - будний?", "Сколько в неделе дней?"};
+
+        //Тексты самих вопросов
+        String[] questionTexts = new String[]{
+                "Какой из указанных дней недели - выходной?",
+                "Какой из указанных дней недели - будний?",
+                "Сколько в неделе дней?"
+        };
+
+        //Индексы правильных ответов
         int[] rightAnswers = new int[]{2, 2, 1};
 
+        //Узнать, сколько вопросов, убедиться что кол-во вопросов и ответов совпадает в массивах
         int questionCount = questionTexts.length;
-        assert(questionCount==rightAnswers.length && questionCount==answerTexts.length);
+        assert (questionCount == rightAnswers.length && questionCount == answerTexts.length);
 
-        Scanner s = new Scanner(System.in);
-        String in = "";
-        String rightAnswer = "";
+        //Создаем сканнер, инициализируем переменные
+        Scanner scanner = new Scanner(System.in);
+        String userInput = "";
+        String currentRightAnswerText = "";
         int rightAnswerCount = 0;
 
-        for(int questionIndex = 0; questionIndex<questionCount; questionIndex++){
-            System.out.println("Вопрос: "+questionTexts[questionIndex]);
+        //Итерируем массив вопросов
+        for (int questionIndex = 0; questionIndex < questionCount; questionIndex++) {
+
+            //Печатаем текст вопроса и ответы
+            System.out.println("Вопрос: " + questionTexts[questionIndex]);
             for (String at : answerTexts[questionIndex]) {
                 System.out.println(at);
             }
-            in = s.next();
-            System.out.println("Ваш ответ: "+in);
-            rightAnswer = answerTexts[questionIndex][rightAnswers[questionIndex]];
-            System.out.println("Правильный ответ: "+rightAnswer);
-            if(rightAnswer.equals(in)){
+
+            //Принимаем ввод пользователя в виде текста
+            userInput = scanner.next();
+            System.out.println("Ваш ответ: " + userInput);
+
+            //Получаем правильный ответ из массивов
+            currentRightAnswerText = answerTexts[questionIndex][rightAnswers[questionIndex]];
+            System.out.println("Правильный ответ: " + currentRightAnswerText);
+
+            //Сравниваем правильный ответ с вводом
+            if (currentRightAnswerText.equals(userInput)) {
                 System.out.println("Правильно!");
                 rightAnswerCount++;
             } else {
                 System.out.println("Неправильно!");
             }
+
         }
-        System.out.println("Правильно отвечено: "+rightAnswerCount);
+
+        System.out.println("Правильно отвечено: " + rightAnswerCount);
     }
 }
--- exercises/N02_Quiz_Alt.java
+++ exercises/N02_Quiz_Alt.java
@@ -1,41 +1,60 @@
 import java.util.Scanner;
 
 public class N02_Quiz_Alt {
+
+    //Альтернативный вариант с вводом через индекс ответа
     public static void main(String[] args) {
-        String[][] answerTexts = new String[][] {
+
+        //Тексты ответов для вопросов
+        String[][] answerTexts = new String[][]{
                 {"Вторник", "Четверг", "Суббота"},
                 {"Суббота", "Воскресенье", "Понедельник"},
                 {"10", "7", "25"}
         };
+
+        //Тексты самих вопросов
         String[] questionTexts = new String[]{
                 "Какой из указанных дней недели - выходной?",
                 "Какой из указанных дней недели - будний?",
                 "Сколько в неделе дней?"
         };
+
+        //Индексы правильных ответов
         int[] rightAnswers = new int[]{2, 2, 1};
 
+        //Узнать, сколько вопросов, убедиться что кол-во вопросов и ответов совпадает в массивах
         int questionCount = questionTexts.length;
-        assert(questionCount==rightAnswers.length && questionCount==answerTexts.length);
+        assert (questionCount == rightAnswers.length && questionCount == answerTexts.length);
 
-        Scanner s = new Scanner(System.in);
-        int in;
+        //Создаем сканнер, инициализируем переменные
+        Scanner scanner = new Scanner(System.in);
+        int userInput;
         int rightAnswerCount = 0;
 
-        for(int questionIndex = 0; questionIndex<questionCount; questionIndex++){
-            System.out.println("Вопрос: "+questionTexts[questionIndex]);
-            for (int x = 0; x<answerTexts.length; x++){
-                System.out.println(x+": "+answerTexts[questionIndex][x]);
+        //Итерируем массив вопросов
+        for (int questionIndex = 0; questionIndex < questionCount; questionIndex++) {
+
+            //Печатаем текст вопроса и ответы с их индексами
+            System.out.println("Вопрос: " + questionTexts[questionIndex]);
+            for (int x = 0; x < answerTexts.length; x++) {
+                System.out.println(x + ": " + answerTexts[questionIndex][x]);
             }
-            in = s.nextInt();
-            System.out.println("Ваш ответ: "+in);
-            System.out.println("Правильный ответ: "+rightAnswers[questionIndex]);
-            if(rightAnswers[questionIndex]==in){
+
+            //Принимаем ввод пользователя в виде ИНДЕКСА
+            userInput = scanner.nextInt();
+            System.out.println("Ваш ответ: " + userInput);
+            System.out.println("Правильный ответ: " + rightAnswers[questionIndex]);
+
+            //Сравниваем правильный ответ с вводом
+            if (rightAnswers[questionIndex] == userInput) {
                 System.out.println("Правильно!");
                 rightAnswerCount++;
             } else {
                 System.out.println("Неправильно!");
             }
+
         }
-        System.out.println("Правильно отвечено: "+rightAnswerCount);
+
+        System.out.println("Правильно отвечено: " + rightAnswerCount);
     }
 }
